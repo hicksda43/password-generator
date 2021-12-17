8e7c7935cc5dcf6c7bee054737d5d51620a71ae3
hicksda43 <93965675+hicksda43@users.noreply.github.com>
2021-12-17 15:13:56 -0500

    fix develop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// slider code
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
